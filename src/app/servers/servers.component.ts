023f3b9262d3b4a656f9c426f03d12da76f3704a
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2020-12-28 17:18:01 -0300

    Using Relative Paths in Programmatic Navigation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,21 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    /* Diferentemente do routerLink o router.navigate não sabe em qual rota estamos atualmente.
+    Por isso se usarmos o caminho relativo que é servers, não temos um erro.
+    Com o relativeTo, podemos dizer ao navigate qual é a rota ativa, com ele passa a ser servers.
+    Assim temos novamente o erro pois tenta acessar servers/servers*/
+    //this.router.navigate(['servers'], {relativeTo: this.route});
   }
 
 }
